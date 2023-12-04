775456e53c8c3b974ec3da5836c87949014e813d
Shawn Sun <32376495+ssz1997@users.noreply.github.com>
2023-12-04 11:25:12 -0800

    Configurable fuse mount path (#92)
    * configurable fuse mount path on host machine
    
    * fix metastore type typo
    
    * mount parent path

--- api/v1alpha1/alluxiocluster_types.go
+++ api/v1alpha1/alluxiocluster_types.go
@@ -143,17 +143,18 @@ type ProxySpec struct {
 }
 
 type FuseSpec struct {
-	Affinity       corev1.Affinity   `json:"affinity,omitempty" yaml:"affinity,omitempty"`
-	Enabled        *bool             `json:"enabled,omitempty" yaml:"enabled,omitempty"`
-	Env            map[string]string `json:"env,omitempty" yaml:"env,omitempty"`
-	Group          *int              `json:"group,omitempty" yaml:"group,omitempty"`
-	JvmOptions     []string          `json:"jvmOptions,omitempty" yaml:"jvmOptions,omitempty"`
-	MountOptions   []string          `json:"mountOptions,omitempty" yaml:"mountOptions,omitempty"`
-	NodeSelector   map[string]string `json:"nodeSelector,omitempty" yaml:"nodeSelector,omitempty"`
-	PodAnnotations map[string]string `json:"podAnnotations,omitempty" yaml:"podAnnotations,omitempty"`
-	Resources      ResourcesSpec     `json:"resources,omitempty" yaml:"resources,omitempty"`
-	Tolerations    []Toleration      `json:"tolerations,omitempty" yaml:"tolerations,omitempty"`
-	User           *int              `json:"user,omitempty" yaml:"user,omitempty"`
+	Affinity         corev1.Affinity   `json:"affinity,omitempty" yaml:"affinity,omitempty"`
+	Enabled          *bool             `json:"enabled,omitempty" yaml:"enabled,omitempty"`
+	Env              map[string]string `json:"env,omitempty" yaml:"env,omitempty"`
+	Group            *int              `json:"group,omitempty" yaml:"group,omitempty"`
+	HostPathForMount string            `json:"hostPathForMount,omitempty" yaml:"hostPathForMount,omitempty"`
+	JvmOptions       []string          `json:"jvmOptions,omitempty" yaml:"jvmOptions,omitempty"`
+	MountOptions     []string          `json:"mountOptions,omitempty" yaml:"mountOptions,omitempty"`
+	NodeSelector     map[string]string `json:"nodeSelector,omitempty" yaml:"nodeSelector,omitempty"`
+	PodAnnotations   map[string]string `json:"podAnnotations,omitempty" yaml:"podAnnotations,omitempty"`
+	Resources        ResourcesSpec     `json:"resources,omitempty" yaml:"resources,omitempty"`
+	Tolerations      []Toleration      `json:"tolerations,omitempty" yaml:"tolerations,omitempty"`
+	User             *int              `json:"user,omitempty" yaml:"user,omitempty"`
 }
 
 type ResourcesSpec struct {
